469cdd9aa86399a2eaea97538acff40b19103fce
l4stflowers <l4stflowers@gmail.com>
2019-05-01 22:49:47 +0900

    Add HomeAsUp navigation to actionbar.

--- app/src/main/java/io/github/l4stflowers/todoapp/MainActivity.kt
+++ app/src/main/java/io/github/l4stflowers/todoapp/MainActivity.kt
@@ -3,6 +3,9 @@ package io.github.l4stflowers.todoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
@@ -13,13 +16,27 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var androidInjector: DispatchingAndroidInjector<Fragment>
 
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         AndroidInjection.inject(this)
-
         setContentView(R.layout.main_act)
+
+        setupActionBar()
     }
 
     override fun supportFragmentInjector() = androidInjector
+
+    private fun setupActionBar() {
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navController = findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
 }
