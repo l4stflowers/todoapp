eb1f6ec923046975c0b4ef5f80a4f8ec922e0ea3
l4stflowers <l4stflowers@gmail.com>
2019-05-04 00:05:55 +0900

    Fix fab control.

--- app/src/main/java/io/github/l4stflowers/todoapp/MainActivity.kt
+++ app/src/main/java/io/github/l4stflowers/todoapp/MainActivity.kt
@@ -2,10 +2,12 @@ package io.github.l4stflowers.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
@@ -24,15 +26,26 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         AndroidInjection.inject(this)
         setContentView(R.layout.main_act)
 
-        setupActionBar()
+        navController = findNavController(this, R.id.nav_host_fragment)
+        setupNavigation(navController)
+        setupActionBar(navController)
     }
 
     override fun supportFragmentInjector() = androidInjector
 
-    private fun setupActionBar() {
+    private fun setupNavigation(navController: NavController) {
+        val fab = findViewById<FloatingActionButton>(R.id.fab_add_task)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.tasksFragment -> fab.isVisible = true
+                else -> fab.isVisible = false
+            }
+        }
+    }
+
+    private fun setupActionBar(navController: NavController) {
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        navController = findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
