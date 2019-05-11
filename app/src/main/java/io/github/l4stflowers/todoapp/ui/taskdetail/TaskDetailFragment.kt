7bc51bfcce5d0b149a7a616cc8bd3c5921e1cb0c
l4stflowers <l4stflowers@gmail.com>
2019-05-11 14:31:06 +0900

    Add task-detail-view's option menu.

--- app/src/main/java/io/github/l4stflowers/todoapp/ui/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/io/github/l4stflowers/todoapp/ui/taskdetail/TaskDetailFragment.kt
@@ -2,6 +2,9 @@ package io.github.l4stflowers.todoapp.ui.taskdetail
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -9,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import dagger.android.support.AndroidSupportInjection
+import io.github.l4stflowers.todoapp.R
 import io.github.l4stflowers.todoapp.databinding.TaskDetailFragBinding
 import javax.inject.Inject
 
@@ -25,6 +29,7 @@ class TaskDetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         AndroidSupportInjection.inject(this)
+        setHasOptionsMenu(true)
         viewDataBinding = TaskDetailFragBinding.inflate(inflater, container, false)
         return viewDataBinding.root
     }
@@ -39,5 +44,28 @@ class TaskDetailFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         taskDetailViewModel.loadTask(args.taskId)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.task_detail_frag_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_done -> {
+                taskDetailViewModel.completeTask()
+                return true
+            }
+            R.id.action_edit -> {
+                // TODO 編集画面への遷移を実装する
+                return true
+            }
+            R.id.action_delete -> {
+                taskDetailViewModel.deleteTask()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
